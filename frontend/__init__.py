b806f8dd68ffd74161c9b388fd6cd59f701dae81
Nathan Chow <nathanwchow@gmail.com>
2020-05-19 19:48:56 -0400

    Turn on remote LED through wifi - working code

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -2,32 +2,24 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 def create_app(test_config=None):
-    # Create and configure Flask application
+    ''' Create and configure Flask application. '''
     app = Flask(__name__)
     app.config.from_mapping(SECRET_KEY='dev')
     socketio = SocketIO(app)
 
-    # Homepage
     @app.route('/')
     def home():
+        ''' Create homepage. '''
         return render_template('home.html.j2')
 
-    # Handle message from javascript client and send broadcast
-    @socketio.on('message')
-    def handle_message(message):
-        print('received message: ' + message)
-        socketio.emit('broadcast', message)     # This will go to all clients
-
-    # Handle and send message from javascript client
-    @socketio.on('add')
-    def handle_add(data):
-        print(data)
-        data = data + 1
-        emit('sum', data)
-
-    # Handle messages from python client
-    @socketio.on('client')
-    def handle_client(message):
-        print(message)
+    # Get ledState from javascript client and broadcast
+    @socketio.on('ledState')
+    def handle_message(ledState):
+        ''' 
+        Handle message containing the next led state from frontend
+        client. 
+        '''
+        # This will go out to all clients contrary to a normal emit()
+        socketio.emit('broadcastLEDState', ledState)
 
     return app
